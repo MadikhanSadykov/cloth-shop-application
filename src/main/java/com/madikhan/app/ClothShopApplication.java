e67a44b32c3c6f0f6cf655586bc99db8e3f720f4
Madikhan Sadykov <madikhansadykov@gmail.com>
2023-03-02 14:03:20 +0600

    first commit

---
+++ src/main/java/com/madikhan/app/ClothShopApplication.java
@@ -0,0 +1,13 @@
+package com.madikhan.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClothShopApplication.class, args);
+    }
+
+}
